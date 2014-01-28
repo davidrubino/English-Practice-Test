b9908b06be080563a524ecff4b47db5e0be2c51d
David <david.rubino@telecomnancy.net>
2014-01-28 20:31:15 +0100

    saisie des questions

--- MainForm/Form1.Designer.cs
+++ MainForm/Form1.Designer.cs
@@ -34,6 +34,9 @@
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.signOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showAnswersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showMyAnswersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showRightAnswersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.Button_Next = new System.Windows.Forms.Button();
@@ -44,9 +47,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.Button_Back = new System.Windows.Forms.Button();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.showAnswersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.showMyAnswersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.showRightAnswersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkResultsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
@@ -57,10 +58,11 @@
             this.fileToolStripMenuItem,
             this.signOutToolStripMenuItem,
             this.showAnswersToolStripMenuItem,
+            this.checkResultsToolStripMenuItem,
             this.aboutToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(442, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(517, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -77,19 +79,19 @@
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.openToolStripMenuItem.Text = "Open";
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.saveToolStripMenuItem.Text = "Save";
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             // 
             // signOutToolStripMenuItem
@@ -97,6 +99,29 @@
             this.signOutToolStripMenuItem.Name = "signOutToolStripMenuItem";
             this.signOutToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
             this.signOutToolStripMenuItem.Text = "Sign in";
+            // 
+            // showAnswersToolStripMenuItem
+            // 
+            this.showAnswersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showMyAnswersToolStripMenuItem,
+            this.showRightAnswersToolStripMenuItem});
+            this.showAnswersToolStripMenuItem.Name = "showAnswersToolStripMenuItem";
+            this.showAnswersToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.showAnswersToolStripMenuItem.Text = "Show Answers";
+            // 
+            // showMyAnswersToolStripMenuItem
+            // 
+            this.showMyAnswersToolStripMenuItem.Name = "showMyAnswersToolStripMenuItem";
+            this.showMyAnswersToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.showMyAnswersToolStripMenuItem.Text = "Show My Answers";
+            this.showMyAnswersToolStripMenuItem.Click += new System.EventHandler(this.showMyAnswersToolStripMenuItem_Click);
+            // 
+            // showRightAnswersToolStripMenuItem
+            // 
+            this.showRightAnswersToolStripMenuItem.Name = "showRightAnswersToolStripMenuItem";
+            this.showRightAnswersToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.showRightAnswersToolStripMenuItem.Text = "Show Right Answers";
+            this.showRightAnswersToolStripMenuItem.Click += new System.EventHandler(this.showRightAnswersToolStripMenuItem_Click);
             // 
             // aboutToolStripMenuItem
             // 
@@ -113,7 +138,7 @@
             "Medium",
             "Hard",
             "Bilingual"});
-            this.comboBox1.Location = new System.Drawing.Point(309, 3);
+            this.comboBox1.Location = new System.Drawing.Point(384, 3);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 1;
@@ -198,34 +223,18 @@
             this.trackBar1.Size = new System.Drawing.Size(104, 45);
             this.trackBar1.TabIndex = 9;
             // 
-            // showAnswersToolStripMenuItem
+            // checkResultsToolStripMenuItem
             // 
-            this.showAnswersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.showMyAnswersToolStripMenuItem,
-            this.showRightAnswersToolStripMenuItem});
-            this.showAnswersToolStripMenuItem.Name = "showAnswersToolStripMenuItem";
-            this.showAnswersToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
-            this.showAnswersToolStripMenuItem.Text = "Show Answers";
-            // 
-            // showMyAnswersToolStripMenuItem
-            // 
-            this.showMyAnswersToolStripMenuItem.Name = "showMyAnswersToolStripMenuItem";
-            this.showMyAnswersToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
-            this.showMyAnswersToolStripMenuItem.Text = "Show My Answers";
-            this.showMyAnswersToolStripMenuItem.Click += new System.EventHandler(this.showMyAnswersToolStripMenuItem_Click);
-            // 
-            // showRightAnswersToolStripMenuItem
-            // 
-            this.showRightAnswersToolStripMenuItem.Name = "showRightAnswersToolStripMenuItem";
-            this.showRightAnswersToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
-            this.showRightAnswersToolStripMenuItem.Text = "Show Right Answers";
-            this.showRightAnswersToolStripMenuItem.Click += new System.EventHandler(this.showRightAnswersToolStripMenuItem_Click);
+            this.checkResultsToolStripMenuItem.Name = "checkResultsToolStripMenuItem";
+            this.checkResultsToolStripMenuItem.Size = new System.Drawing.Size(92, 20);
+            this.checkResultsToolStripMenuItem.Text = "Check Results";
+            this.checkResultsToolStripMenuItem.Click += new System.EventHandler(this.checkResultsToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 353);
+            this.ClientSize = new System.Drawing.Size(517, 353);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.Button_Back);
             this.Controls.Add(this.label1);
@@ -269,6 +278,7 @@
         private System.Windows.Forms.ToolStripMenuItem showAnswersToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem showMyAnswersToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem showRightAnswersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkResultsToolStripMenuItem;
     }
 }
 
--- MainForm/Form1.cs
+++ MainForm/Form1.cs
@@ -70,10 +70,129 @@ namespace MainForm
             Answer q3_a3 = new Answer("51", false);
             Question q3 = new Question("How many states are in the United States?", q3_a1, q3_a2, q3_a3);
 
+            //Question 4
+            Answer q4_a1 = new Answer("Alaska", false);
+            Answer q4_a2 = new Answer("Hawaii", true);
+            Answer q4_a3 = new Answer("New Mexico", false);
+            Question q4 = new Question("What was the last state to enter the Union?", q4_a1, q4_a2, q4_a3);
+
+            //Question 5
+            Answer q5_a1 = new Answer("John F. Kennedy", false);
+            Answer q5_a2 = new Answer("Martin Luther King Jr.", true);
+            Answer q5_a3 = new Answer("Bob Kennedy", false);
+            Question q5 = new Question("What man acted a lot for the progress of the Civil Rights in the 60s?", q5_a1, q5_a2, q5_a3);
+
+            //Question 6
+            Answer q6_a1 = new Answer("7 Red Stripes for the 7 Confederate States", false);
+            Answer q6_a2 = new Answer("Red and Blue for the 2 main political parties", false);
+            Answer q6_a3 = new Answer("13 Red and White Stripes for the 13 Colonies", true);
+            Question q6 = new Question("Which sentence reflects the correct meaning of the US Flag?", q6_a1, q6_a2, q6_a3);
+
+            //Question 7
+            Answer q7_a1 = new Answer("Gold City", false);
+            Answer q7_a2 = new Answer("Cash City", false);
+            Answer q7_a3 = new Answer("Sin City", true);
+            Question q7 = new Question("What is the nickname of Las Vegas?", q7_a1, q7_a2, q7_a3);
+
+            //Question 8
+            Answer q8_a1 = new Answer("Missouri", false);
+            Answer q8_a2 = new Answer("Alabama", false);
+            Answer q8_a3 = new Answer("New York", true);
+            Question q8 = new Question("Civil War – Find the intruder:", q8_a1, q8_a2, q8_a3);
+
+            //Question 9
+            Answer q9_a1 = new Answer("North Dakota", false);
+            Answer q9_a2 = new Answer("South Dakota", true);
+            Answer q9_a3 = new Answer("Colorado", false);
+            Question q9 = new Question("Where is Mount Rushmore located?", q9_a1, q9_a2, q9_a3);
+
+            //Question 10
+            Answer q10_a1 = new Answer("The Star Spangled Banner", true);
+            Answer q10_a2 = new Answer("The Stars and Stripes Forever", false);
+            Answer q10_a3 = new Answer("God Bless America", false);
+            Question q10 = new Question("What is the National Anthem’s name?", q10_a1, q10_a2, q10_a3);
+
+            //Question 11
+            Answer q11_a1 = new Answer("In God We Trust", true);
+            Answer q11_a2 = new Answer("God Bless America", false);
+            Answer q11_a3 = new Answer("E Pluribus Unum", false);
+            Question q11 = new Question("What sentence is written on every American bill?", q11_a1, q11_a2, q11_a3);
+
+            //Question 12
+            Answer q12_a1 = new Answer("George Washington, John Adams, Thomas Jefferson", false);
+            Answer q12_a2 = new Answer("Jimmy Carter, Ronald Reagan, George H.W. Bush", false);
+            Answer q12_a3 = new Answer("Dwight D. Eisenhower, Harry S. Truman, John F. Kennedy", true);
+            Question q12 = new Question("The following presidents succeeded one to each other in office, except for:", q12_a1, q12_a2, q12_a3);
+
+            //Question 13
+            Answer q13_a1 = new Answer("Los Angeles", true);
+            Answer q13_a2 = new Answer("San Francisco", false);
+            Answer q13_a3 = new Answer("Seattle", false);
+            Question q13 = new Question("What city is famous for being home to many movies companies?", q13_a1, q13_a2, q13_a3);
+
+            //Question 14
+            Answer q14_a1 = new Answer("The Mormon Church", false);
+            Answer q14_a2 = new Answer("The Southern Baptist Convention", false);
+            Answer q14_a3 = new Answer("The Methodist Church", true);
+            Question q14 = new Question("Which church was NOT founded in America?", q14_a1, q14_a2, q14_a3);
+
+            //Question 15
+            Answer q15_a1 = new Answer("Monument Valley", true);
+            Answer q15_a2 = new Answer("The Grand Canyon", false);
+            Answer q15_a3 = new Answer("Death Valley", false);
+            Question q15 = new Question("What was John Ford’s favorite place to shot movies?", q15_a1, q15_a2, q15_a3);
+
+            //Question 16
+            Answer q16_a1 = new Answer("Tories and Democrats", false);
+            Answer q16_a2 = new Answer("Republicans and Democrats", true);
+            Answer q16_a3 = new Answer("Libertarians and Tories", false);
+            Question q16 = new Question("What are the main political parties in the United States?", q16_a1, q16_a2, q16_a3);
+
+            //Question 17
+            Answer q17_a1 = new Answer("July 4 1776", true);
+            Answer q17_a2 = new Answer("July 4 1789", false);
+            Answer q17_a3 = new Answer("July 14 1776", false);
+            Question q17 = new Question("When was the Declaration of Independence signed?", q17_a1, q17_a2, q17_a3);
+
+            //Question 18
+            Answer q18_a1 = new Answer("The Declaration of Independence", false);
+            Answer q18_a2 = new Answer("The Bill of Rights", false);
+            Answer q18_a3 = new Answer("The Constitution", true);
+            Question q18 = new Question("What is the Supreme Law of the Land?", q18_a1, q18_a2, q18_a3);
+
+            //Question 19
+            Answer q19_a1 = new Answer("Winter Wonderland", false);
+            Answer q19_a2 = new Answer("Let It Snow", false);
+            Answer q19_a3 = new Answer("White Christmas", true);
+            Question q19 = new Question("Irving Berlin, author of the song God Bless America, also wrote the favorite Christmas song:", q19_a1, q19_a2, q19_a3);
+
+            //Question 20
+            Answer q20_a1 = new Answer("Labor Day", false);
+            Answer q20_a2 = new Answer("Halloween", true);
+            Answer q20_a3 = new Answer("Memorial Day", false);
+            Question q20 = new Question("Which of the following is NOT a federal holiday in the US:", q20_a1, q20_a2, q20_a3);
+
             //Add questions to the list
             this.questionsList.Add(q1);
             this.questionsList.Add(q2);
             this.questionsList.Add(q3);
+            this.questionsList.Add(q4);
+            this.questionsList.Add(q5);
+            this.questionsList.Add(q6);
+            this.questionsList.Add(q7);
+            this.questionsList.Add(q8);
+            this.questionsList.Add(q9);
+            this.questionsList.Add(q10);
+            this.questionsList.Add(q11);
+            this.questionsList.Add(q12);
+            this.questionsList.Add(q13);
+            this.questionsList.Add(q14);
+            this.questionsList.Add(q15);
+            this.questionsList.Add(q16);
+            this.questionsList.Add(q17);
+            this.questionsList.Add(q18);
+            this.questionsList.Add(q19);
+            this.questionsList.Add(q20);
             return questionsList;
         }
 
@@ -110,6 +229,18 @@ namespace MainForm
                 content = "There are no answers yet.";
             }
             return content;
+        }
+
+        private string CheckResults()
+        {
+            int score = 0;
+            int totalAnswers = this.rightAnswers.Count;
+            for(int i=0; i<this.userAnswers.Count; i++)
+            {
+                if (this.rightAnswers.Contains(this.userAnswers.ElementAt(i)))
+                    score++;
+            }
+            return "Correct answers: " + score + " out of " + totalAnswers;
         }
         
         private void Form1_Load(object sender, EventArgs e)
@@ -163,6 +294,11 @@ namespace MainForm
         private void showMyAnswersToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show(this.ShowUserAnswers(), "My Answers", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+        private void checkResultsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this.CheckResults(), "My Results", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
         }
     }
 }
